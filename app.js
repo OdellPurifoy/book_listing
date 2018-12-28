742c2bb44aa9baddfae64ed66203d6b8de4d4178
Odell <odell.purifoy@gmail.com>
2018-12-28 16:10:17 -0500

    Built js Add book functionality

--- app.js
+++ app.js
@@ -36,5 +36,27 @@ class UI {
       <td>$(book.isbn)</td>
       <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
     `;
+
+    list.appendChild(row);
   }
 }
+
+//Event to display books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+//Event to add a book
+document.querySelector("#book-form").addEventListener("submit", e => {
+  //prevent actual submit
+  e.preventDefault();
+
+  //Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  //Instantiate books
+  const book = new Book(title, author, isbn);
+
+  //Add book to UI
+  UI.addBookToList(book);
+});
