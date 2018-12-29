aea8a70ab39580d64b6bcf1cd67402314861ebb6
Odell <odell.purifoy@gmail.com>
2018-12-28 21:47:54 -0500

    Added alert messaging

--- app.js
+++ app.js
@@ -46,6 +46,19 @@ class UI {
     }
   }
 
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+
+    //Vanish in 3 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  }
+
   static clearFields() {
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
@@ -68,7 +81,7 @@ document.querySelector("#book-form").addEventListener("submit", e => {
 
   //Validate fields
   if (title === "" || author === "" || isbn === "") {
-    alert("Please fill in all fields");
+    UI.showAlert("Please fill in all fields", "danger");
   } else {
     //Instantiate books
     const book = new Book(title, author, isbn);
@@ -76,8 +89,11 @@ document.querySelector("#book-form").addEventListener("submit", e => {
     //Add book to UI
     UI.addBookToList(book);
 
+    //Show success message
+    UI.showAlert("Book Added Succesfully", "success");
+
     //Clear field
-    IU.clearFields();
+    UI.clearFields();
   }
 });
 
